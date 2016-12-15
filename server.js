0748e437cd56be41952a12ffacbe0343056fc5a4
kappisan <wilkosz.ian@gmail.com>
2016-12-15 22:53:46 +0000

    serverside pricing

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const PORT=7777;
 
 var express = require('express');
 var fs = require('fs');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var moment = require('moment');
+
+
+
+
+/* config */
 
 var app = express();
 
 app.use(express.static('./'));
-
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -15,15 +20,60 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 })); 
 
 
-var recipes = [];
+/* stocks */
+var kappisan = {
+	price: 240,
+	currency: "GBX",
+	valuations: []
+};
 
 
-app.get('/api/recipes', function (req, res) {
+/* api */
+
+var price = (Math.random() * 10) + 4100;
+var multiplier = Math.random() - 0.5;
+var startDate = moment("01-Jan-14", "DD-MMM-YY");
+var i = 0;
+var data = [];
+
+setInterval(function() {
+
+    if(i % 4 == 0) { multiplier = Math.random() - 0.5; }
+
+    price += (Math.random() * multiplier);
+    var date = startDate.add(1, "days")
+
+    //data.push({date: date.format("DD-MMM-YY"), close: price});    
+
+	console.log("price update ---  £", price);
+
+	i++;
+
+}, 1000)
 
 
-	res.send(recipes);
+
+
+
+
+
+
+
+
+
+
+
+
+
+var users = [];
+
+app.get('/api/users', function (req, res) {
+
+	res.send(users);
 });
 
+
+/* start server */
 
 server = app.listen(PORT);
 
