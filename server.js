15378452bd4556cef6d8527ab8854705a25ae63d
kappisan <wilkosz.ian@gmail.com>
2016-12-17 01:00:25 +0000

    emit price from server

--- server.js
+++ server.js
@@ -3,12 +3,57 @@ var express = require('express');
 var app = express();  
 var server = require('http').createServer(app);  
 var io = require('socket.io')(server);
+var moment = require('moment');
 
 app.use(express.static(__dirname + '/'));  
 
 app.get('/', function(req, res,next) {  
     res.sendFile(__dirname + '/index.html');
 });
+
+
+/* stocks */
+var kappisan = {
+	price: 240,
+	currency: "GBX",
+	valuations: []
+};
+
+
+
+/* api */
+
+var price = (Math.random() * 10) + 4100;
+var multiplier = Math.random() - 0.5;
+var startDate = moment("01-Jan-14", "DD-MMM-YY");
+var i = 0;
+var data = [];
+
+// price changes every 3s
+setInterval(function() {
+
+    if(i % 4 == 0) { multiplier = Math.random() - 0.5; }
+
+    price += (Math.random() * multiplier);
+    var date = startDate.add(1, "days")
+
+    //data.push({date: date.format("DD-MMM-YY"), close: price});    
+
+	console.log("price update ---  £", price);
+
+	i++;
+
+}, 3000)
+
+
+
+
+
+
+
+
+
+/* socket.io */
 
 io.on('connection', function(client) {  
     console.log('Client connected...');
@@ -17,67 +62,27 @@ io.on('connection', function(client) {
         console.log(data);
     });
 
+	setInterval(function() {
+		client.emit("price", price);
+	}, 3000);
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
 
 server.listen(7777);  
 
 // const PORT=7777; 
-
-// var express = require('express');
-// var fs = require('fs');
-// var bodyParser = require('body-parser');
-// var moment = require('moment');
-
-// var server = require('http').createServer(app);  
-// var io = require('socket.io')(server);
-
-
-
-// /* config */
-
-// var app = express();
-
-// app.use(express.static('./'));
-
-// app.use( bodyParser.json() );       // to support JSON-encoded bodies
-// app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-//   extended: true
-// })); 
-
-
-// /* stocks */
-// var kappisan = {
-// 	price: 240,
-// 	currency: "GBX",
-// 	valuations: []
-// };
-
-
-// /* api */
-
-// var price = (Math.random() * 10) + 4100;
-// var multiplier = Math.random() - 0.5;
-// var startDate = moment("01-Jan-14", "DD-MMM-YY");
-// var i = 0;
-// var data = [];
-
-// setInterval(function() {
-
-//     if(i % 4 == 0) { multiplier = Math.random() - 0.5; }
-
-//     price += (Math.random() * multiplier);
-//     var date = startDate.add(1, "days")
-
-//     //data.push({date: date.format("DD-MMM-YY"), close: price});    
-
-// 	console.log("price update ---  £", price);
-
-// 	i++;
-
-// }, 3000)
-
-
-
 
 
 
